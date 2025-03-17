aa186a2a27dd5822defc11bcfbe1ffc2617e4d28
PedroSales007 <pedro.santos013@sptech.school>
2025-03-17 15:01:30 -0300

    Att. modificado o codigo pra lux

--- Grupo/Arduino_Oficial.ino
+++ Grupo/Arduino_Oficial.ino
@@ -1,24 +1,83 @@
-const int LDR_PIN = A0;  // Pino do LDR
-int valorLDR; // Criação de variavel do valor da luminosidade 
-float lux; // variavel do lux em decimal
+int ldr_pin = A0;
+
+int ldr_read = 0;
+
+float vin = 5.00;
+
+float valor_ADC = 0.00488758, r_ohms = 10000;
+
+ 
 
 void setup() {
-  Serial.begin(9600); // função que recebe parametros de velocidade de transmição de bits
+
+  Serial.begin(9600);
+
 }
 
+ 
+
 void loop() {
-  valorLDR = analogRead(LDR_PIN);  // Leitura do valor recebido
 
-  
-  lux = (5 * valorLDR) * 1.5 ;  // A constante de calibração deve ser ajustada;  Fórmula para converter LDR para Lux (ajuste conforme sua calibração;
-  Serial.print("LuxMaximo:");
-  Serial.print(8000);
-  Serial.print(" ");
-  Serial.print("LuxMinimo:");
-  Serial.print(4000);
-  Serial.print(" ");
-  Serial.print("LuxAtual:");
-  Serial.println(lux);
+  if(isnan(ldr_read)){
 
-  delay(1000);  // Atualiza a cada 1 segundo
+    Serial.println("Erro ao ler o sensor");
+
+  }
+
+  else{
+
+    ldr_read = analogRead(ldr_pin);
+
+    float vout = valor_ADC * ldr_read;
+
+    float res_ldr = (r_ohms * (vin - vout))/vout;
+
+    float lux = 500/(res_ldr/1000);
+
+ 
+
+    if(ldr_read > 750){
+
+      Serial.print(ldr_read);
+
+      Serial.print(" Vout: ");
+
+      Serial.print(vout);
+
+      Serial.print(" R_ldr: ");
+
+      Serial.print(res_ldr);
+
+      Serial.print(" Lux: ");
+
+      Serial.print(lux);
+
+      Serial.println(" Claro");
+
+    }
+
+    else{
+
+      Serial.print(ldr_read);
+
+      Serial.print(" Vout: ");
+
+      Serial.print(vout);
+
+      Serial.print(" R_ldr: ");
+
+      Serial.print(res_ldr);
+
+      Serial.print(" Lux: ");
+
+      Serial.print(lux);
+
+      Serial.println(" Escuro");
+
+    }
+
+  }
+
+  delay(2000);
+
 }
